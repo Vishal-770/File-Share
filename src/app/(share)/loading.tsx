cc708c18e12f79805e4e91982498c40d3e2c267d
Vishal-770 <vishalpeace07@gmail.com>
2025-06-15 22:43:21 +0530

    fixed Loading Animation in share page

--- src/app/(share)/loading.tsx
+++ src/app/(share)/loading.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
 import React from "react";
 
 const loading = () => {
-  return <div>Loading</div>;
+  return (
+ <div className="flex items-center justify-center min-h-[100-vh]">
+        <Image src="/loading.gif" alt="loading" height={50} width={50} />
+      </div>
+  );
 };
 
 export default loading;
